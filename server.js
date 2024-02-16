db8c5407245f259a1ed3e926f5a89f44fe5fae76
Mar-Issah <masy370@gmail.com>
2024-02-16 15:44:37 -0600

    Update server.js file

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ import startServer from './libs/boot';
 import injectRoutes from './routes';
 
 const server = express();
-const port = process.env.PORT || 5000;
 
-// Middleware to parse JSON bodies
 server.use(express.json({ limit: '200mb' }));
 
 injectRoutes(server);
