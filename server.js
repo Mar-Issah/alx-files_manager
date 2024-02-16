74e4bf209e673dd6cfa8602dace64c7f85076b7e
Mar-Issah <masy370@gmail.com>
2024-02-16 15:38:44 -0600

    Update server.js file

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
 import express from 'express';
-// import startServer from './libs/boot';
+import startServer from './libs/boot';
 import injectRoutes from './routes';
-// import injectMiddlewares from './libs/middlewares';
 
 const server = express();
 const port = process.env.PORT || 5000;
 
-// injectMiddlewares(server);
 // Middleware to parse JSON bodies
 server.use(express.json({ limit: '200mb' }));
 
 injectRoutes(server);
 
-// startServer(server);
-// const startServer = (api) => {
-//   envLoader();
-//   const port = process.env.PORT || 5000;
-//   const env = process.env.NPM_LIFECYCLE_EVENT || 'dev';
-//   api.listen(port, () => {
-//     console.log(`[${env}] API has started listening at port:${port}`);
-//   });
-// };
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+startServer(server);
 
 export default server;
